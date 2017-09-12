a9af63e521f02de3cba7b682ce760e170610af3d
Yura <yustetsic@gmail.com>
2017-09-12 15:09:51 +0300

    Clear extentions

--- app/src/main/java/org/zamahaka/cheremosh/Extentions.kt
+++ app/src/main/java/org/zamahaka/cheremosh/Extentions.kt
@@ -6,8 +6,6 @@ import android.arch.lifecycle.Observer
 import android.view.View
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
-import com.zamahaka.wotsapp.mvp.ActiveView
-import com.zamahaka.wotsapp.mvp.BasePresenter
 import retrofit2.Call
 
 /**
@@ -24,12 +22,6 @@ inline fun <T> Call<T>.enqueue(init: CallbackHolder<T>.() -> Unit) {
 }
 
 fun ImageView.load(url: Url) = Picasso.with(context).load(url).fit().centerInside().into(this)
-
-val ActiveView?.isValid: Boolean get() = this?.isActive ?: false
-
-inline fun <V : ActiveView> BasePresenter<V>.performViewOperation(operation: V.() -> Unit) {
-    if (view.isValid) view?.operation()
-}
 
 fun View.visible() {
     visibility = View.VISIBLE
