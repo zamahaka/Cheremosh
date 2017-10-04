d6c9b314f5bd297953572cf21323f391d8ecd7f1
Yura <yustetsic@gmail.com>
2017-10-04 15:57:36 +0300

    Add timeline view setup

---
+++ app/src/main/java/org/zamahaka/cheremosh/ui/timeline/TimeLineActivity.kt
@@ -0,0 +1,13 @@
+package org.zamahaka.cheremosh.ui.timeline
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.zamahaka.cheremosh.R
+
+class TimeLineActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_time_line)
+    }
+}
