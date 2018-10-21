9abf024ba307c659b9907dc09fcc7f6408ac2986
Yura <yustetsic@gmail.com>
2018-10-21 13:28:03 +0300

    Refactor NotesListViewModel.

--- app/src/main/java/org/zamahaka/cheremosh/ui/notes/NotesListViewModel.kt
+++ app/src/main/java/org/zamahaka/cheremosh/ui/notes/NotesListViewModel.kt
@@ -1,32 +1,18 @@
 package org.zamahaka.cheremosh.ui.notes
 
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import android.util.Log
-import com.google.firebase.database.*
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.FirebaseDatabase
+import org.zamahaka.cheremosh.domain.FirebaseLiveData
+import org.zamahaka.cheremosh.extensions.listOf
 import org.zamahaka.cheremosh.model.NoteFile
 
-class NotesListViewModel(
+class NotesListViewModel : ViewModel() {
 
-) : ViewModel() {
-
-    val notesFiles = MutableLiveData<List<NoteFile>>().apply {
-        value = emptyList()
-    }
-
-    init {
-        FirebaseDatabase.getInstance().getReference("notes").addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onCancelled(p0: DatabaseError) {
-                Log.d("myLog", "onCancelled: ${p0.message}")
-            }
-
-            override fun onDataChange(p0: DataSnapshot) {
-                val notes = p0.getValue(
-                        object : GenericTypeIndicator<List<@JvmSuppressWildcards NoteFile?>>() {}
-                )?.filterNotNull().orEmpty()
-                notesFiles.postValue(notes)
-            }
-        })
-    }
+    val notesFiles: LiveData<List<NoteFile>> = FirebaseLiveData(
+            reference = FirebaseDatabase.getInstance().getReference("notes"),
+            convert = DataSnapshot::listOf
+    )
 
 }
